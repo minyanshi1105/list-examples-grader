09687eed5185eb0a4ec715e4c524009b718ebe5f
minyanshi1105 <114315303+minyanshi1105@users.noreply.github.com>
2022-11-18 12:27:41 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,4 +4,14 @@ set -e
 
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission/
+FILE=ListExamples.java
 
+if [[ -f "$FILE" ]]
+then
+        echo "ListExamples.java exists!"
+else
+        echo "Cannot find ListExamples.java!"
+        echo "Your score is 0 out of 3!"
+        exit
+fi
