6589861737fa40687b0c333fb91ffa31ba9ff7f7
minyanshi1105 <114315303+minyanshi1105@users.noreply.github.com>
2022-11-18 12:32:56 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -15,3 +15,30 @@ else
         echo "Your score is 0 out of 3!"
         exit
 fi
+
+cp ../TestListExamples.java ./
+
+set +e
+
+SCORE=0
+
+javac -cp ".;../lib/*" ListExamples.java TestListExamples.java
+
+if [[ $? -eq 0 ]]
+then
+  SCORE=$(($SCORE+1))
+else
+  echo "Your score is" $SCORE "out of 3!"
+  exit
+fi
+
+FAILED=$(java -cp ".;../lib/*" org.junit.runner.JUnitCore TestListExamples | grep -oP "(?<=,  Failures: )[0-9]+")
+
+if [[ $? -eq 1 ]]
+then
+  SCORE=$(($SCORE+2))
+else
+  SCORE=$(($SCORE+2-$FAILED))
+fi
+
+echo "Your score is" $SCORE "out of 3!"
